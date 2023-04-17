d65c64ceb92d36accd73b16e9069c5853b5d8695
BernardoBoffa-Molinar <62026029+BernardoBoffa-Molinar@users.noreply.github.com>
2023-04-17 14:07:50 +0100

    Initial commit

---
+++ Source/BulletFarm/BulletFarmGameMode.cpp
@@ -0,0 +1,14 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "BulletFarmGameMode.h"
+#include "BulletFarmCharacter.h"
+#include "UObject/ConstructorHelpers.h"
+
+ABulletFarmGameMode::ABulletFarmGameMode()
+	: Super()
+{
+	// set default pawn class to our Blueprinted character
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnClassFinder(TEXT("/Game/FirstPerson/Blueprints/BP_FirstPersonCharacter"));
+	DefaultPawnClass = PlayerPawnClassFinder.Class;
+
+}
